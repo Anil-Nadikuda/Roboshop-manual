fbcae875a3321d30ce6862637acaa85af2bad90d
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2023-12-26 11:57:22 +0530

    update done

---
+++ delete_logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source_dir="/tmp/shellscript_logs"
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $source_dir ]
+then
+    echo -e"$R Source directory: $source_dir doesnot exist $N"
+fi
+
+FILES_TO_DELETE= $(find . -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting files:$line"
+
+does
+
+done <<< $FILES_TO_DELETE
